e21c4c552b3001572680e0358490ef837702b69c
dorteks <ayodelealayemi@gmail.com>
2022-06-04 05:21:11 +0100

    comments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,20 +16,29 @@ class Counter extends Component {
   render() {
     return (
       <div>
+        {/* passing children from counters */}
         {this.props.children}
+
+        {/* code destructuring   */}
         <span className={this.getBadgeClasses()}>{this.state.count}</span>
+
+        {/* increment button */}
         <button
           onClick={this.hanldeIncrement}
           className="btn btn-secondary btn-small m-2"
         >
           Increment
         </button>
+
+        {/* decrement button */}
         <button
           onClick={this.handleDecrement}
           className="btn btn-secondary btn-small m-2"
         >
           Decrement
         </button>
+
+        {/* delete button */}
         <button
           onClick={() => this.props.onDelete(this.props.id)}
           className="btn btn-danger btn-small"
