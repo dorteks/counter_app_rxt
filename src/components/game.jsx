c52db08963b21c6025425ad3019d72a2bd216ad4
dorteks <ayodelealayemi@gmail.com>
2022-06-06 07:36:03 +0100

    moved onclick function to board

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,28 +1,39 @@
 import React, { Component } from "react";
 
 class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
       <button
         className="btn btn-primary btn-big m-1"
-        onClick={() => this.setState({ value: "X" })}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
