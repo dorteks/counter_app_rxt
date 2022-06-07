3b1a5bcf6e5a0057ba5ced869a7a391700b86d57
dorteks <ayodelealayemi@gmail.com>
2022-06-07 06:47:37 +0100

    shot myself in the leg

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 // import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Game from "./components/game";
+import Counters from "./components/counters";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Counters />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
